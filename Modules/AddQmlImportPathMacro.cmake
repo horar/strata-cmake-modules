4cfa40196c39806d9473559627113c987e72988c
Lubomir Carik <lubomir.carik@onsemi.com>
2021-08-05 17:33:40 +0200

    cmake: introduce CMake macro to remove QML import path from variable used by QtCreator

--- Modules/AddQmlImportPathMacro.cmake
+++ Modules/AddQmlImportPathMacro.cmake
@@ -6,8 +6,21 @@ macro(add_qml_import_path)
         set(QML_DIRS "${QML_IMPORT_PATH}")
         LIST(APPEND QML_DIRS ${local_PATH})
 
-        ## Additional import path used to resolve QML modules in Qt Creator's code model
+        # Additional import path used to resolve QML modules in Qt Creator's code model
         set(QML_IMPORT_PATH "${QML_DIRS}" CACHE STRING "Qt Creator extra qml import paths" FORCE)
         message(STATUS "...updated QML import path with: '${local_PATH}'")
+    endif()
+endmacro()
+
+macro(remove_qml_import_path)
+    set(qmlImportPath PATH)
+    cmake_parse_arguments(local "" "${qmlImportPath}" "" ${ARGN})
+
+    if(${local_PATH} IN_LIST QML_IMPORT_PATH)
+        list(REMOVE_ITEM QML_IMPORT_PATH ${local_PATH})
+
+        # Additional import path used to resolve QML modules in Qt Creator's code model
+        set(QML_IMPORT_PATH "${QML_DIRS}" CACHE STRING "Qt Creator extra qml import paths" FORCE)
+        message(STATUS "...removed QML import path: '${local_PATH}'")
     endif()
 endmacro()
